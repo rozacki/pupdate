89942ac4df353091565709a6b31d3b66ec88ab82
rozackic <chris.rozacki@valtech.co.uk>
2015-09-28 09:48:58 +0100

    unified error handling

--- JobContext.go
+++ JobContext.go
@@ -7,21 +7,19 @@ type JobContext struct{
 	JobDataChannel chan *JobData
 	//inherited from Task
 	Debug bool
-	//how many rows have been affected based on what driver returns
-	RowsAffected uint64
 	//current data
 	TaskName string
 }
 
 //TaskConfiguration extands existing MonitoringModule
-func (this* JobContext) EventStartJob()(*MonitoringError){
-	return Monitoring.Trace(this.TaskName,StartJob)
+func (this* JobContext) EventStartJob()(error){
+	return GMonitoring.Trace(this.TaskName,StartJob)
 }
 
-func (this* JobContext) EventStopTJob()(*MonitoringError){
-	return Monitoring.Trace(this.TaskName,StopJob)
+func (this* JobContext) EventStopTJob()(error){
+	return GMonitoring.Trace(this.TaskName,StopJob)
 }
 
-func (this* JobContext) Event(data interface{})(*MonitoringError){
-	return Monitoring.Trace(this.TaskName,Trace)
+func (this* JobContext) Event(data interface{})(error){
+	return GMonitoring.Trace(this.TaskName,Trace)
 }
